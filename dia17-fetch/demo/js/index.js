fae106da17998838ef597852f89456be95922e7c
bgomezdh <bgomez@digitalhouse.com>
2023-11-02 17:47:37 -0300

    agregando dia 17 sobre fetch

---
+++ dia17-fetch/demo/js/index.js
@@ -0,0 +1,6 @@
+/* El ENDPOINT de la API */
+let url ='https://randomuser.me/api/';
+
+/* Caputando elementos del DOM */
+
+
---
+++ dia17-fetch/demo/js/provincias.js
@@ -0,0 +1,6 @@
+/* El ENDPOINT de la API */
+let provincias = 'https://apis.datos.gob.ar/georef/api/provincias';
+
+/* Caputando elementos del DOM */
+
+
